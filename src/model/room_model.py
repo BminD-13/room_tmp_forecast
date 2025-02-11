7bdf7657629a9eb3cea8e820d797f6676758b71a
Benjamin_Daiber <benjamin.daiber@uni-ulm.de>
2025-02-11 09:42:48 +0100

    add orthogonaliyt of the sun

--- src/model/room_model.py
+++ src/model/room_model.py
@@ -1,4 +1,5 @@
 # model
+import numpy as np
 import csv
 
 class RaumModell:
@@ -53,39 +54,73 @@ class RaumModell:
             y_new[i] = (1 - alpha) * y[i] + alpha * (u if i == 0 else y_new[i - 1])
         return y_new
 
-    def orthogonalität(self, azimuth, elevation, normale):
+    def orthogonalität(azimuth, elevation, surface_azimuth, surface_tilt):
         """
-        Berechnet die pojizierte Fläche einer Wand,
-        welche von der Sonne bestrahlt wird
+        Berechnet die projizierte Fläche einer Wand für eine Liste von Sonnenpositionen.
 
         Parameter:
-        azimuth:   Himmelsrichtung der Sonne
-        elevation: Winkel der Sonne vom Boden gemessen
-        normale:   Flächennormale der betrachteten Oberfläche
+        azimuth: Array der Sonnenazimutwinkel (0° = Norden, 90° = Osten, ...)
+        elevation: Array der Sonnenhöhenwinkel über dem Horizont (0° = Horizont, 90° = Zenit)
+        surface_azimuth: Ausrichtung der Oberfläche (0° = Norden, 90° = Osten, ...)
+        surface_tilt: Neigungswinkel der Oberfläche (0° = vertikal, 90° = horizontal)
+
+        Rückgabe:
+        Array mit der Projektion der Sonnenstrahlung auf die Fläche (Werte zwischen 0 und 1)
         """
-        return azimuth, elevation
+
+        # Umwandlung in Radiant für alle Werte
+        azimuth, elevation = np.radians(azimuth), np.radians(elevation)
+        surface_azimuth, surface_tilt = np.radians(surface_azimuth), np.radians(surface_tilt)
+
+        # Sonnenvektoren (vektorisiert)
+        sun_x = np.cos(elevation) * np.cos(azimuth)
+        sun_y = np.cos(elevation) * np.sin(azimuth)
+        sun_z = np.sin(elevation)
+
+        # Flächennormalen (skalar, da konstant)
+        normal_x = np.cos(surface_tilt) * np.cos(surface_azimuth)
+        normal_y = np.cos(surface_tilt) * np.sin(surface_azimuth)
+        normal_z = np.sin(surface_tilt)
+
+        # Skalarprodukt (vektorisiert)
+        ortho = sun_x * normal_x + sun_y * normal_y + sun_z * normal_z
+
+        return np.maximum(0, ortho)  # Keine negativen Werte (wenn die Fläche im Schatten liegt)
+
+
     
     def sonnenstrahlung(self, tmp, sonnenleistung, orthogonalität):
         return tmp + sonnenleistung * orthogonalität # * self.fensterfaktor
     
     def raumtemperatur_model(self, tmp_0, tmp_aussen, sonnenleistung, orthogonalität):
-        tmp_wand =  	[tmp_aussen[0]] * self.n_wand
-        tmp_speicher =  [tmp_aussen[0]] * self.n_speicher
-        tmp_raum =      [tmp_0] * self.n_raum
+        tmp_wall =  	[tmp_aussen[0]] * self.n_wand
+        tmp_storage =  [tmp_aussen[0]] * self.n_speicher
+        tmp_room =      [tmp_0] * self.n_raum
         
         ergebnisse = []
         for i in range(len(tmp_aussen)):
 
-            tmp_wand = self.ptn(tmp_wand, tmp_aussen[i], self.tau_wand, self.n_wand)
-            tmp_wand = self.sonnenstrahlung(tmp_wand, sonnenleistung[i], orthogonalität[i])
+            tmp_wall = self.ptn(tmp_wall, tmp_aussen[i], self.tau_wand, self.n_wand)
+            tmp_wall = self.sonnenstrahlung(tmp_wall, sonnenleistung[i], orthogonalität[i])
             
-            tmp_speicher = self.ptn(tmp_speicher, tmp_raum[-1], self.tau_speicher, self.n_speicher)
-            tmp_speicher = self.sonnenstrahlung(tmp_speicher, sonnenleistung[i], orthogonalität[i])
+            tmp_storage = self.ptn(tmp_storage, tmp_room[-1], self.tau_speicher, self.n_speicher)
+            tmp_storage = self.sonnenstrahlung(tmp_storage, sonnenleistung[i], orthogonalität[i])
             
-            tmp_raum = self.ptn(tmp_raum, tmp_speicher[-1], self.tau_raum, self.n_raum)
-            tmp_raum = self.ptn(tmp_raum, tmp_wand[-1], self.tau_raum, self.n_raum)
-            tmp_raum = self.sonnenstrahlung(tmp_raum, sonnenleistung[i], orthogonalität[i])
+            tmp_room = self.ptn(tmp_room, tmp_storage[-1], self.tau_raum, self.n_raum)
+            tmp_room = self.ptn(tmp_room, tmp_wall[-1], self.tau_raum, self.n_raum)
+            tmp_room = self.sonnenstrahlung(tmp_room, sonnenleistung[i], orthogonalität[i])
             
-            ergebnisse.append(tmp_raum[-1])
+            ergebnisse.append(tmp_room[-1])
         
         return ergebnisse
+    
+    def run_model(self, dataset):
+
+        sunOrtho = self.orthogonalität(dataset["azimuth"], dataset["elevation"], 180, 0)
+
+        return  self.raumtemperatur_model(
+                    tmp_0          = 21.5357487923,
+                    tmp_aussen     = dataset["tmpAmbient"],
+                    sonnenleistung = dataset["sunPower"],
+                    orthogonalität = sunOrtho
+                )
